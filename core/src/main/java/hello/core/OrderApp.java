508d04e9f3feaddd15a1a2e6613892e4b2138de6
michael <mskim_451@dongguk.edu>
2022-05-16 14:15:34 +0900

    김영한.스프링_핵심_원리_기본편.~빈_생명주기_콜백, 김영한.스프링_MVC1.HTTP_요청_데이터_개요
    학습 내용
    1. 웹 애플리케이션 이해(웹 서버, WAS, 서블릿, 스레드 등)
    2. HTTP서블릿 이용 방법
    
    느낀점
    1. 서블릿 컨테이너와 스프링 컨테이너 둘다 같은 목적에 의해 만들어짐. 객체를 효율적으로 관리하기 위해.

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -4,18 +4,27 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class MemberApp {
+public class OrderApp {
+
     public static void main(String[] args) {
+
         ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = ac.getBean("memberService", MemberService.class);
-        Member member = new Member(1L, "memberA", Grade.VIP);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Member findMember = memberService.findMember(1L);
-        System.out.println("new member = " + member.getName());
-        System.out.println("find member = " + findMember.getName());
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
+
+        System.out.println("order = " + order);
+        System.out.println("order = " + order.calculatePrice());
     }
 }
